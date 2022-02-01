9535174d417c558d1bfbb8a1224af92c69829573
Youngseo Choi <ys550499@gmail.com>
2022-02-01 14:40:14 +0900

    currency 추가

--- src/main/scala/com/easywritten/allowancechart/domain/Money.scala
+++ src/main/scala/com/easywritten/allowancechart/domain/Money.scala
@@ -21,6 +21,15 @@ final case class Money(currency: Currency, amount: MoneyAmount) {
 object Money {
   def usd(amount: MoneyAmount): Money = Money(Currency.USD, amount)
   def krw(amount: MoneyAmount): Money = Money(Currency.KRW, amount)
+  def jpy(amount: MoneyAmount): Money = Money(Currency.JPY, amount)
+  def cny(amount: MoneyAmount): Money = Money(Currency.CNY, amount)
+  def hkd(amount: MoneyAmount): Money = Money(Currency.HKD, amount)
+  def eur(amount: MoneyAmount): Money = Money(Currency.EUR, amount)
+  def sgd(amount: MoneyAmount): Money = Money(Currency.SGD, amount)
+  def cad(amount: MoneyAmount): Money = Money(Currency.CAD, amount)
+  def chf(amount: MoneyAmount): Money = Money(Currency.CHF, amount)
+  def aud(amount: MoneyAmount): Money = Money(Currency.AUD, amount)
+  def gbp(amount: MoneyAmount): Money = Money(Currency.GBP, amount)
 
   implicit val eqMoney: Eq[Money] = Eq.fromUniversalEquals
 }
@@ -30,6 +39,15 @@ sealed trait Currency extends EnumEntry
 object Currency extends Enum[Currency] {
   case object USD extends Currency
   case object KRW extends Currency
+  case object JPY extends Currency
+  case object CNY extends Currency
+  case object HKD extends Currency
+  case object EUR extends Currency
+  case object SGD extends Currency
+  case object CAD extends Currency
+  case object CHF extends Currency
+  case object AUD extends Currency
+  case object GBP extends Currency
 
   override def values: IndexedSeq[Currency] = findValues
 
