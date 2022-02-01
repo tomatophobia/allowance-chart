ba7fc59c6f6c866199059cd831feecaef9d3d867
Youngseo Choi <ys550499@gmail.com>
2022-02-01 14:35:30 +0900

    원화, 외화를 모두 입금할 수 있음

--- src/test/scala/com/easywritten/allowancechart/domain/AccountSpec.scala
+++ src/test/scala/com/easywritten/allowancechart/domain/AccountSpec.scala
@@ -6,13 +6,30 @@ import zio.test._
 
 object AccountSpec extends DefaultRunnableSpec {
   def spec: ZSpec[Environment, Failure] =
-    suite("AccountSpec") {
+    suite("AccountSpec")(
       test("Deposit usd into Account") {
-        val acc1 = Account()
+        val acc = Account()
         val usd = Money.usd(1235)
-        acc1.deposit(usd)
+        acc.deposit(usd)
         val mb = MoneyBag.fromMoneys(usd)
-        assertTrue(acc1.balance == mb)
+        assertTrue(acc.balance == mb)
+      },
+      test("Deposit multiple times") {
+        val acc = Account()
+        acc.deposit(Money.usd(123.12))
+        acc.deposit(Money.usd(456.45))
+        acc.deposit(Money.krw(12519))
+        acc.deposit(Money.krw(56947))
+
+        val expectedMb =
+          MoneyBag(
+            Map[Currency, MoneyAmount](
+              Currency.USD -> BigDecimal(579.57),
+              Currency.KRW -> BigDecimal(69466)
+            )
+          )
+
+        assertTrue(acc.balance == expectedMb)
       }
-    }
+    )
 }
