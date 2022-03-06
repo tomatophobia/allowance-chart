3a8bbd007c1db8f45ffefe2ebacb1fb53d24fa00
Youngseo Choi <ys550499@gmail.com>
2022-03-06 23:32:31 +0900

    wip : multipartBody를 이용해 파일 입력 받기, wartremover 에러 발생...

--- src/main/scala/com/easywritten/allowancechart/adapter/in/TransactionHistoryEndpoints.scala
+++ src/main/scala/com/easywritten/allowancechart/adapter/in/TransactionHistoryEndpoints.scala
@@ -3,6 +3,8 @@ package com.easywritten.allowancechart.adapter.in
 import cats.syntax.all._
 import com.easywritten.allowancechart.adapter.in.page.RegisterTransactionHistory
 import io.circe.generic.auto._
+import sttp.model.Part
+import sttp.tapir.{CodecFormat, Endpoint, Schema}
 import sttp.tapir.json.circe._
 import sttp.tapir.generic.auto._
 import sttp.tapir.ztapir._
@@ -11,13 +13,32 @@ import zio._
 object TransactionHistoryEndpoints {
 
   // TODO Error를 스트링 대신 다른 것으로
-  val registerPage: ZServerEndpoint[Env, Unit, String, String] =
+  val getRegisterPage: ZServerEndpoint[Env, Unit, String, String] =
     endpoint.get
-      .in("transaction-history" / "register")
-      .errorOut(stringBody)
+      .in("transaction-history" / "register-page")
       .out(htmlBodyUtf8)
+      .errorOut(stringBody)
+      .tag(ApiDocTag.transactionHistory)
+      .summary("Transaction history register page")
       .zServerLogic { _ =>
         UIO.succeed(RegisterTransactionHistory.html)
+      }
+
+  final case class NameWithTransactionHistory(name: String, transactionHistory: Part[TapirFile])
+
+  val registerTransactionHistory: ZServerEndpoint[Env, NameWithTransactionHistory, String, Unit] =
+    endpoint.post
+      .in("transaction-history")
+      .in(multipartBody[NameWithTransactionHistory])
+      .errorOut(stringBody)
+      .tag(ApiDocTag.transactionHistory)
+      .summary("Register transaction history file")
+      .zServerLogic { case NameWithTransactionHistory(name, transactionHistoryPart) =>
+        println(name)
+        println(transactionHistoryPart.name)
+        println(transactionHistoryPart.otherDispositionParams)
+        println(transactionHistoryPart.headers)
+        UIO.unit
       }
 
   final case class Pet(species: String, url: String)
@@ -35,18 +56,10 @@ object TransactionHistoryEndpoints {
         }
       }
 
-  val htmlExampleEndpoint: ZServerEndpoint[Env, Int, String, String] =
-    endpoint
-      .in("example" / path[Int]("hello"))
-      .errorOut(stringBody)
-      .out(htmlBodyUtf8)
-      .zServerLogic { x =>
-        UIO.succeed(RegisterTransactionHistory.html)
-      }
-
   val all: List[ZServerEndpoint[Env, _, _, _]] =
     List(
-      registerPage
+      getRegisterPage,
+      registerTransactionHistory
     )
 
   type Env = Any
