bbe1c7fb092abb8b1a90c8fb7c843fb4d915887a
Youngseo Choi <ys550499@gmail.com>
2022-05-23 21:31:16 +0900

    여러 개로 나뉜 파싱 테스트 케이스를 하나로 합치고 fixture를 분리함

--- src/test/scala/com/easywritten/allowancechart/adapter/in/TransactionRecordParserSpec.scala
+++ src/test/scala/com/easywritten/allowancechart/adapter/in/TransactionRecordParserSpec.scala
@@ -1,6 +1,7 @@
 package com.easywritten.allowancechart.adapter.in
 
 import com.easywritten.allowancechart.application.port.in.TransactionRecord
+import com.easywritten.allowancechart.application.service.ServiceError
 import com.easywritten.allowancechart.domain.{Holding, Money, MoneyBag, Nation, SecuritiesCompany, Stock}
 import zio._
 import zio.test._
@@ -14,120 +15,16 @@ object TransactionRecordParserSpec extends DefaultRunnableSpec {
   override def spec: ZSpec[Environment, Failure] = {
     suite("TransactionRecordParserSpec")(
       suite("parse data using schema")(
-        suite("Daishin")(
-          testM("deposit") {
-            val data = List(
-              "2020.10.12",
-              "입금",
-              "",
-              "500,000",
-              "",
-              "",
-              "",
-              "",
-              "0",
-              "",
-              "",
-              "",
-              "종합투자상품",
-              "1",
-              "개별상품대체입금",
-              "",
-              "",
-              "",
-              "",
-              "",
-              "",
-              "",
-              "",
-              "",
-              "0",
-              "500,000"
-            )
-            val record = parseDaishin(daishinSchema, data)
-            val expected = TransactionRecord.Deposit(LocalDate.of(2020, 10, 12), "입금", Money.krw(500000), "개별상품대체입금")
-            assertM(record)(equalTo(expected))
-          },
-          testM("Foreign Exchange Buy") {
-            val data = List(
-              "2020.10.12",
-              "입금",
-              "KRW",
-              "499,995",
-              "",
-              "1,150.42",
-              "",
-              "",
-              "0",
-              "",
-              "",
-              "",
-              "",
-              "2",
-              "외화매수환전",
-              "USD",
-              "434.62",
-              "",
-              "",
-              "",
-              "",
-              "",
-              "",
-              "",
-              "434.62",
-              "5"
-            )
-            val record = parseDaishin(daishinSchema, data)
-            val expected = TransactionRecord.ForeignExchangeBuy(
-              LocalDate.of(2020, 10, 12),
-              "입금",
-              MoneyBag.fromMoneys(Money.krw(-499995), Money.usd(434.62)),
-              1150.42,
-              "외화매수환전"
-            )
-            assertM(record)(equalTo(expected))
-          },
-          testM("Buy") {
-            val data = List(
-              "2020.11.4",
-              "해외증권장내매매",
-              "USD",
-              "325",
-              "",
-              "",
-              "IVV",
-              "1",
-              "1",
-              "",
-              "",
-              "",
-              "",
-              "1",
-              "현금매수",
-              "",
-              "",
-              "",
-              "",
-              "Ishares Core S&P 500 Etf",
-              "325",
-              "0.26",
-              "",
-              "",
-              "109.36",
-              "5"
-            )
-            val record = parseDaishin(daishinSchema, data)
-            val expected = TransactionRecord.Buy(
-              LocalDate.of(2020, 11, 4),
-              "해외증권장내매매",
-              Money.usd(325),
-              Holding(Stock("IVV", Nation.USA), Money.usd(325), 1),
-              "현금매수",
-              Money.usd(0.26)
-            )
-            assertM(record)(equalTo(expected))
+        testM("Daishin") {
+          daishinFixture.toList.foldLeft[IO[ServiceError, TestResult]](assertCompletesM) {
+            case (accM, (data, expected)) =>
+              val record = parseDaishin(daishinSchema, data)
+              for {
+                ass <- assertM(record)(equalTo(expected))
+                acc <- accM
+              } yield ass && acc
           }
-        )
+        }
       ),
       suite("parse transaction record file")(
         testM("Nonghyup")(
@@ -175,4 +72,105 @@ object TransactionRecordParserSpec extends DefaultRunnableSpec {
     "원화예수금"
   )
 
+  val daishinFixture: Map[Seq[String], TransactionRecord] = Map(
+    List(
+      "2020.10.12",
+      "입금",
+      "",
+      "500,000",
+      "",
+      "",
+      "",
+      "",
+      "0",
+      "",
+      "",
+      "",
+      "종합투자상품",
+      "1",
+      "개별상품대체입금",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "0",
+      "500,000"
+    ) -> TransactionRecord.Deposit(LocalDate.of(2020, 10, 12), "입금", Money.krw(500000), "개별상품대체입금"),
+    List(
+      "2020.10.12",
+      "입금",
+      "KRW",
+      "499,995",
+      "",
+      "1,150.42",
+      "",
+      "",
+      "0",
+      "",
+      "",
+      "",
+      "",
+      "2",
+      "외화매수환전",
+      "USD",
+      "434.62",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "434.62",
+      "5"
+    )
+      -> TransactionRecord.ForeignExchangeBuy(
+        LocalDate.of(2020, 10, 12),
+        "입금",
+        MoneyBag.fromMoneys(Money.krw(-499995), Money.usd(434.62)),
+        1150.42,
+        "외화매수환전"
+      ),
+    List(
+      "2020.11.4",
+      "해외증권장내매매",
+      "USD",
+      "325",
+      "",
+      "",
+      "IVV",
+      "1",
+      "1",
+      "",
+      "",
+      "",
+      "",
+      "1",
+      "현금매수",
+      "",
+      "",
+      "",
+      "",
+      "Ishares Core S&P 500 Etf",
+      "325",
+      "0.26",
+      "",
+      "",
+      "109.36",
+      "5"
+    )
+      -> TransactionRecord.Buy(
+        LocalDate.of(2020, 11, 4),
+        "해외증권장내매매",
+        Money.usd(325),
+        Holding(Stock("IVV", Nation.USA), Money.usd(325), 1),
+        "현금매수",
+        Money.usd(0.26)
+      )
+  )
 }
